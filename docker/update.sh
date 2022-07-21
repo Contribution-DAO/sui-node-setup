e089dd76de922e43b975c69f4ae7dd1f151e59ea
Nunoy <83507970+NunoyHaxxana@users.noreply.github.com>
2022-07-21 08:16:05 +0700

    Update update.sh

--- docker/update.sh
+++ docker/update.sh
@@ -30,15 +30,6 @@ docker-compose up -d
 
 
 
-cd $HOME && rm -rf sui
-mkdir sui && cd sui
-wget -qO docker-compose.yaml https://raw.githubusercontent.com/MystenLabs/sui/main/docker/fullnode/docker-compose.yaml
-wget -qO fullnode-template.yaml https://github.com/MystenLabs/sui/raw/main/crates/sui-config/data/fullnode-template.yaml
-wget -qO genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
-sed -i 's/127.0.0.1/0.0.0.0/' fullnode-template.yaml
-docker-compose down --volumes
-docker-compose pull
-docker-compose up -d
 
 
 echo "==========================================================================================================================="    
