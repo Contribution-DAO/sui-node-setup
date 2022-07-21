bdd67c7880675df29a346630e5dc73c6a1fd15c8
Nunoy <83507970+NunoyHaxxana@users.noreply.github.com>
2022-07-21 08:15:44 +0700

    Update update.sh

--- docker/update.sh
+++ docker/update.sh
@@ -1,3 +1,35 @@
+#!/bin/bash
+echo "==========================================================================================================================="
+echo -e "\033[0;35m"
+echo "  ██████╗ ██████╗ ███╗   ██╗████████╗██████╗ ██╗██████╗ ██╗   ██╗████████╗██╗ ██████╗ ███╗   ██╗██████╗  █████╗  ██████╗ ";
+echo " ██╔════╝██╔═══██╗████╗  ██║╚══██╔══╝██╔══██╗██║██╔══██╗██║   ██║╚══██╔══╝██║██╔═══██╗████╗  ██║██╔══██╗██╔══██╗██╔═══██╗";
+echo " ██║     ██║   ██║██╔██╗ ██║   ██║   ██████╔╝██║██████╔╝██║   ██║   ██║   ██║██║   ██║██╔██╗ ██║██║  ██║███████║██║   ██║";
+echo " ██║     ██║   ██║██║╚██╗██║   ██║   ██╔══██╗██║██╔══██╗██║   ██║   ██║   ██║██║   ██║██║╚██╗██║██║  ██║██╔══██║██║   ██║";
+echo " ╚██████╗╚██████╔╝██║ ╚████║   ██║   ██║  ██║██║██████╔╝╚██████╔╝   ██║   ██║╚██████╔╝██║ ╚████║██████╔╝██║  ██║╚██████╔╝";
+echo "  ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝   ╚═╝   ╚═╝  ╚═╝╚═╝╚═════╝  ╚═════╝    ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ";
+																													  
+echo -e "\033[0;35m"
+echo "==========================================================================================================================="                                                                                    
+sleep 1
+
+
+echo -e "\e[1m\e[32mUpdating Sui fullnode... \e[0m" && sleep 1
+
+cd $HOME/sui
+sudo docker-compose down --volumes
+cd $HOME && sudo rm -rf sui
+sudo mkdir sui && cd sui
+wget https://raw.githubusercontent.com/MystenLabs/sui/main/docker/fullnode/docker-compose.yaml
+wget https://github.com/MystenLabs/sui/raw/main/crates/sui-config/data/fullnode-template.yaml
+wget https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
+sudo cp fullnode-template.yaml fullnode.yaml
+sudo sed -i 's/127.0.0.1/0.0.0.0/' fullnode.yaml
+sudo sed -i 's/fullnode-template.yaml/fullnode.yaml/' docker-compose.yaml
+docker-compose pull
+docker-compose up -d
+
+
+
 cd $HOME && rm -rf sui
 mkdir sui && cd sui
 wget -qO docker-compose.yaml https://raw.githubusercontent.com/MystenLabs/sui/main/docker/fullnode/docker-compose.yaml
@@ -5,4 +37,19 @@ wget -qO fullnode-template.yaml https://github.com/MystenLabs/sui/raw/main/crate
 wget -qO genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
 sed -i 's/127.0.0.1/0.0.0.0/' fullnode-template.yaml
 docker-compose down --volumes
+docker-compose pull
 docker-compose up -d
+
+
+echo "==========================================================================================================================="    
+
+
+
+Container_ID=$(docker ps -q -f name=sui-fullnode-1)
+result=$( docker inspect -f {{.State.Status}} $Container_ID)
+if [ $result = "running" ]
+then
+echo -e "Your Sui node \e[32mUpdate and running normally\e[39m!"
+else
+echo -e "Your Sui node \e[31mUpdate failed installed\e[39m, Please Re-install."
+fi
