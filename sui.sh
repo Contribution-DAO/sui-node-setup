9ba6de3e2b25d4ced237551df235fa73050c0b84
Nunoy <83507970+NunoyHaxxana@users.noreply.github.com>
2022-08-25 21:20:10 +0700

    Update sui.sh

--- sui.sh
+++ sui.sh
@@ -21,46 +21,58 @@ echo -e "\e[1m\e[32m2. Install dependencies \e[0m" && sleep 1
 sudo apt-get update && DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get install -y --no-install-recommends tzdata git ca-certificates curl build-essential libssl-dev pkg-config libclang-dev cmake jq
 sudo apt install libprotobuf-dev protobuf-compiler
 
-echo -e "\e[1m\e[32m3. Install Rust \e[0m" && sleep 1
-sudo curl https://sh.rustup.rs -sSf | sh -s -- -y
-source $HOME/.cargo/env
+#echo -e "\e[1m\e[32m3. Install Rust \e[0m" && sleep 1
+#sudo curl https://sh.rustup.rs -sSf | sh -s -- -y
+#source $HOME/.cargo/env
 
-echo -e "\e[1m\e[32m4. Install Sui \e[0m" && sleep 1
-sudo mkdir -p /var/sui/db
-cd $HOME
-git clone https://github.com/MystenLabs/sui.git
-cd sui
-git remote add upstream https://github.com/MystenLabs/sui
-git fetch upstream
-git checkout --track upstream/devnet
-wget -O /var/sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
-sudo cp crates/sui-config/data/fullnode-template.yaml /var/sui/fullnode.yaml
-cargo build --release -p sui-node
-sudo mv ~/sui/target/release/sui-node /usr/local/bin/
-sudo cp /var/sui/fullnode.yaml /var/sui/fullnode.yaml.bak
-sudo sed -i 's/suidb/\/var\/sui\/db/'  /var/sui/fullnode.yaml
-sudo sed -i 's/127.0.0.1/0.0.0.0/'  /var/sui/fullnode.yaml
-sudo sed -i 's/genesis.blob/\/var\/sui\/genesis.blob/' /var/sui/fullnode.yaml
+#echo -e "\e[1m\e[32m4. Install Sui \e[0m" && sleep 1
+#sudo mkdir -p /var/sui/db
+#cd $HOME
+#git clone https://github.com/MystenLabs/sui.git
+#cd sui
+#git remote add upstream https://github.com/MystenLabs/sui
+#git fetch upstream
+#git checkout --track upstream/devnet
+#wget -O /var/sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
+#sudo cp crates/sui-config/data/fullnode-template.yaml /var/sui/fullnode.yaml
+#cargo build --release -p sui-node
+#sudo mv ~/sui/target/release/sui-node /usr/local/bin/
+#sudo cp /var/sui/fullnode.yaml /var/sui/fullnode.yaml.bak
+#sudo sed -i 's/suidb/\/var\/sui\/db/'  /var/sui/fullnode.yaml
+#sudo sed -i 's/127.0.0.1/0.0.0.0/'  /var/sui/fullnode.yaml
+#sudo sed -i 's/genesis.blob/\/var\/sui\/genesis.blob/' /var/sui/fullnode.yaml
+
+echo -e "\e[1m\e[32m3. Update Configs \e[0m" && sleep 1
+cd $HOME 
+sudo mkdir -p sui_node
+cd $HOME/sui_node
+wget -O genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
+wget -O fullnode.yaml https://github.com/MystenLabs/sui/raw/main/crates/sui-config/data/fullnode-template.yaml
+sudo sed -i 's/suidb/\$HOME\/sui_node\/db/'  $HOME/sui_node/fullnode.yaml
+sudo sed -i 's/127.0.0.1/0.0.0.0/'  $HOME/sui_node/fullnode.yaml
+sudo sed -i 's/genesis.blob/\$HOME\/sui_node\/genesis.blob/' $HOME/sui_node/fullnode.yaml
+
+
+echo -e "\e[1m\e[32m4. Download Sui Binaries \e[0m" && sleep 1
+version=$(wget -qO- https://api.github.com/repos/SecorD0/Sui/releases/latest | jq -r ".tag_name")
+wget -qO- "https://github.com/SecorD0/Sui/releases/download/${version}/sui-linux-amd64-${version}.tar.gz" | sudo tar -C /usr/local/bin/ -xzf -
 
 
 echo -e "\e[1m\e[32m5. Make Sui Service \e[0m" && sleep 1
-echo "[Unit]
-Description=Sui Node
-After=network.target
+sudo tee /etc/systemd/system/suid.service > /dev/null <<EOF
+[Unit]
+Description=Sui node
+After=network-online.target
 
 [Service]
 User=$USER
-Type=simple
-ExecStart=/usr/local/bin/sui-node --config-path /var/sui/fullnode.yaml
+ExecStart=$(which sui-node) --config-path $HOME/sui_node/fullnode.yaml
 Restart=on-failure
+RestartSec=3
 LimitNOFILE=65535
 
 [Install]
-WantedBy=multi-user.target" > $HOME/suid.service
-mv $HOME/suid.service /etc/systemd/system/
-
-sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
-Storage=persistent
+WantedBy=multi-user.target
 EOF
 
 sudo systemctl restart systemd-journald
