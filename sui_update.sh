cdae08cc55ec1ff92071b6aeb2023c050fc8d109
Nunoy <83507970+NunoyHaxxana@users.noreply.github.com>
2022-08-31 17:05:17 +0700

    Update sui_update.sh

--- sui_update.sh
+++ sui_update.sh
@@ -31,6 +31,21 @@ version=$(wget -qO- https://api.github.com/repos/SecorD0/Sui/releases/latest | j
 wget -qO- "https://github.com/SecorD0/Sui/releases/download/${version}/sui-linux-amd64-${version}.tar.gz" | sudo tar -C /usr/local/bin/ -xzf -
 systemctl start suid
 
+echo "==========================================================================================================================="    
+
+echo -e '\e[32mCheck your sui status\e[39m' && sleep 1
+if [[ `service suid status | grep active` =~ "running" ]]; then
+  echo -e "Your Sui node \e[32minstalled and running normally\e[39m!"
+else
+  echo -e "Your Sui node \e[31mwas failed installed\e[39m, Please Re-install."
+fi
+
+echo -e "\e[1m\e[32m6. Usefull commands \e[0m" && sleep 1
+echo -e "Check your node logs: \e[1m\e[32m journalctl -fu suid -o cat \e[0m"
+echo -e "Check your node status: \e[1m\e[32m sudo service suid status \e[0m"
+echo -e "Restart your node: \e[1m\e[32m sudo systemctl restart suid \e[0m"
+echo -e "Stop your node: \e[1m\e[32m sudo systemctl stop suid \e[0m"
+echo -e "Start your node: \e[1m\e[32m sudo systemctl start suid \e[0m"
 
 
 
