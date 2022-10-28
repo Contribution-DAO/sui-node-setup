9faee877c93f1a014b603038f110527b785a86e9
Nunoy <83507970+NunoyHaxxana@users.noreply.github.com>
2022-10-28 12:47:18 +0700

    Update sui_update.sh

--- sui_update.sh
+++ sui_update.sh
@@ -20,19 +20,52 @@ sleep 1
 
 
 echo -e "\e[1m\e[32m Update Sui fullnode \e[0m" && sleep 1
-cd $HOME/sui_node
-sudo systemctl stop suid
-cd $HOME/sui_node
+#cd $HOME/sui_node
+#sudo systemctl stop suid
+#cd $HOME/sui_node
 #sudo rm -rf $HOME/sui_node/db
+#wget -O genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
+#wget -O fullnode.yaml https://github.com/MystenLabs/sui/raw/main/crates/sui-config/data/fullnode-template.yaml
+#sudo sed -i 's/127.0.0.1/0.0.0.0/'  $HOME/sui_node/fullnode.yaml
+#sudo yq -i '.network-address = "/ip4/0.0.0.0/tcp/8080/http"' $HOME/sui_node/fullnode.yaml
+#sudo yq -i ".genesis.genesis-file-location = \"$HOME/sui_node/genesis.blob\"" $HOME/sui_node/fullnode.yaml
+#sudo yq -i ".db-path = \"$HOME/sui_node/db\"" $HOME/sui_node/fullnode.yaml
+#version=$(wget -qO- https://api.github.com/repos/SecorD0/Sui/releases/latest | jq -r ".tag_name")
+#wget -qO- "https://github.com/SecorD0/Sui/releases/download/${version}/sui-linux-amd64-${version}.tar.gz" | sudo tar -C /usr/local/bin/ -xzf -
+#sudo systemctl start suid
+#sudo systemctl stop suid
+
+cd $HOME
+sudo systemctl stop suid
+rm -rf sui
+git clone https://github.com/MystenLabs/sui.git
+cd sui
+git remote add upstream https://github.com/MystenLabs/sui
+git fetch upstream
+git checkout -B devnet --track upstream/devnet
+cargo build -p sui-node -p sui --release
+mv ~/sui/target/release/sui-node /usr/local/bin/
+mv ~/sui/target/release/sui /usr/local/bin/
+
+
+rm -rf $HOME/sui_node/db
+
+
+
+cd $HOME/sui_node
 wget -O genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
 wget -O fullnode.yaml https://github.com/MystenLabs/sui/raw/main/crates/sui-config/data/fullnode-template.yaml
 sudo sed -i 's/127.0.0.1/0.0.0.0/'  $HOME/sui_node/fullnode.yaml
 sudo yq -i '.network-address = "/ip4/0.0.0.0/tcp/8080/http"' $HOME/sui_node/fullnode.yaml
 sudo yq -i ".genesis.genesis-file-location = \"$HOME/sui_node/genesis.blob\"" $HOME/sui_node/fullnode.yaml
 sudo yq -i ".db-path = \"$HOME/sui_node/db\"" $HOME/sui_node/fullnode.yaml
-version=$(wget -qO- https://api.github.com/repos/SecorD0/Sui/releases/latest | jq -r ".tag_name")
-wget -qO- "https://github.com/SecorD0/Sui/releases/download/${version}/sui-linux-amd64-${version}.tar.gz" | sudo tar -C /usr/local/bin/ -xzf -
-sudo systemctl start suid
+
+
+
+sudo systemctl restart suid
+
+
+
 
 echo "==========================================================================================================================="    
 
