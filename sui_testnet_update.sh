f4979f1fd146e705dc2b6c397be1eacffb8d2ba6
Nunoy <83507970+NunoyHaxxana@users.noreply.github.com>
2023-02-02 11:37:35 +0700

    Update sui_testnet_update.sh

--- sui_testnet_update.sh
+++ sui_testnet_update.sh
@@ -40,10 +40,10 @@ mv ~/sui/target/release/sui /usr/local/bin/
 
 cd $HOME/.sui
 wget -O genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/testnet/genesis.blob
-wget -O fullnode.yaml https://github.com/MystenLabs/sui/raw/main/crates/sui-config/data/fullnode-template.yaml
-sed -i 's/127.0.0.1/0.0.0.0/'  $HOME/.sui/fullnode.yaml
-sed -i "s|db-path:.*|db-path: $HOME/.sui/db|g" $HOME/.sui/fullnode.yaml
-sed -i "s|genesis-file-location:.*|genesis-file-location: $HOME/.sui/genesis.blob|g" $HOME/.sui/fullnode.yaml
+#wget -O fullnode.yaml https://github.com/MystenLabs/sui/raw/main/crates/sui-config/data/fullnode-template.yaml
+#sed -i 's/127.0.0.1/0.0.0.0/'  $HOME/.sui/fullnode.yaml
+#sed -i "s|db-path:.*|db-path: $HOME/.sui/db|g" $HOME/.sui/fullnode.yaml
+#sed -i "s|genesis-file-location:.*|genesis-file-location: $HOME/.sui/genesis.blob|g" $HOME/.sui/fullnode.yaml
 
 
 
