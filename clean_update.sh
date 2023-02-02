6573feb92d84b7e31756f11a02095989ba31af85
Nunoy <83507970+NunoyHaxxana@users.noreply.github.com>
2023-02-02 19:39:53 +0700

    Create clean_update.sh

---
+++ clean_update.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+echo -e "\033[0;33m"
+echo "==========================================================================================================================="
+echo " "
+echo "  ██████╗ ██████╗ ███╗   ██╗████████╗██████╗ ██╗██████╗ ██╗   ██╗████████╗██╗ ██████╗ ███╗   ██╗██████╗  █████╗  ██████╗ ";
+echo " ██╔════╝██╔═══██╗████╗  ██║╚══██╔══╝██╔══██╗██║██╔══██╗██║   ██║╚══██╔══╝██║██╔═══██╗████╗  ██║██╔══██╗██╔══██╗██╔═══██╗";
+echo " ██║     ██║   ██║██╔██╗ ██║   ██║   ██████╔╝██║██████╔╝██║   ██║   ██║   ██║██║   ██║██╔██╗ ██║██║  ██║███████║██║   ██║";
+echo " ██║     ██║   ██║██║╚██╗██║   ██║   ██╔══██╗██║██╔══██╗██║   ██║   ██║   ██║██║   ██║██║╚██╗██║██║  ██║██╔══██║██║   ██║";
+echo " ╚██████╗╚██████╔╝██║ ╚████║   ██║   ██║  ██║██║██████╔╝╚██████╔╝   ██║   ██║╚██████╔╝██║ ╚████║██████╔╝██║  ██║╚██████╔╝";
+echo "  ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝   ╚═╝   ╚═╝  ╚═╝╚═╝╚═════╝  ╚═════╝    ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ";
+                                                                                                                                                                                                 
+echo -e "\033[0;33m"
+echo "==========================================================================================================================="                                                                                    
+sleep 1
+
+
+
+
+echo -e "\e[1m\e[32m Update Sui fullnode \e[0m" && sleep 1
+source $HOME/.cargo/env
+
+cd $HOME
+sudo systemctl stop suid
+rm -rf sui
+git clone https://github.com/MystenLabs/sui.git
+cd sui
+git remote add upstream https://github.com/MystenLabs/sui
+git fetch upstream
+git checkout -B testnet --track upstream/testnet
+cargo build -p sui-node -p sui --release
+mv ~/sui/target/release/sui-node /usr/local/bin/
+mv ~/sui/target/release/sui /usr/local/bin/
+
+
+
+
+rm -rf $HOME/.sui/db
+
+
+
+cd $HOME/.sui
+wget -O genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/testnet/genesis.blob
+#wget -O fullnode.yaml https://github.com/MystenLabs/sui/raw/main/crates/sui-config/data/fullnode-template.yaml
+#sed -i 's/127.0.0.1/0.0.0.0/'  $HOME/.sui/fullnode.yaml
+#sed -i "s|db-path:.*|db-path: $HOME/.sui/db|g" $HOME/.sui/fullnode.yaml
+#sed -i "s|genesis-file-location:.*|genesis-file-location: $HOME/.sui/genesis.blob|g" $HOME/.sui/fullnode.yaml
+
+
+
+
+sudo systemctl restart suid
+
+
+
+
+echo "==========================================================================================================================="    
+
+echo -e '\e[32mCheck your sui status\e[39m' && sleep 1
+if [[ `service suid status | grep active` =~ "running" ]]; then
+  echo -e "Your Sui node \e[32minstalled and running normally\e[39m!"
+else
+  echo -e "Your Sui node \e[31mwas failed installed\e[39m, Please Re-install."
+fi
+
+echo " "
+echo -e "\e[1m\e[34mYour Sui Version : $(/usr/local/bin/sui-node -V)\e[0m" && sleep 1
+echo " "
+echo " "
+echo " "
+
+echo -e "\e[1m\e[32m6. Usefull commands \e[0m" && sleep 1
+echo -e "Check your node logs: \e[1m\e[32m journalctl -fu suid -o cat \e[0m"
+echo -e "Check your node status: \e[1m\e[32m sudo service suid status \e[0m"
+echo -e "Restart your node: \e[1m\e[32m sudo systemctl restart suid \e[0m"
+echo -e "Stop your node: \e[1m\e[32m sudo systemctl stop suid \e[0m"
+echo -e "Start your node: \e[1m\e[32m sudo systemctl start suid \e[0m"
